eba0fef263336535645a720649976d205bae4026
weversonneri <weversonneri@outlook.com>
2021-10-23 18:24:35 -0300

    :sparkles: create signin component

---
+++ src/components/SignInBox/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View } from 'react-native';
+import { COLORS } from '../../theme';
+import { Button } from '../Button';
+
+import { styles } from './styles';
+
+export function SignInBox() {
+  return (
+    <View style={styles.container}>
+
+      <Button
+        title="Entrar com Github"
+        color={COLORS.BLACK_PRIMARY}
+        backgroundColor={COLORS.YELLOW}
+        icon="github"
+      />
+
+    </View>
+  );
+}
