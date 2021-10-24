353c71b73c91840fb664062d4d22fd273bad9399
weversonneri <weversonneri@outlook.com>
2021-10-24 01:08:34 -0300

    :sparkles: list messages w socket.io
    update messages list real-time using socket.io

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -2,12 +2,36 @@ import React, { useEffect, useState } from 'react';
 import { ScrollView } from 'react-native';
 import { api } from '../../services/api';
 
+import { io } from 'socket.io-client';
+
 import { Message, MessageProps } from '../Message';
 
 import { styles } from './styles';
 
+const messagesQueue: MessageProps[] = [];
+
+const socket = io(String(api.defaults.baseURL));
+socket.on('new_message', (newMessage) => {
+  messagesQueue.push(newMessage);
+});
+
 export function MessageList() {
   const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setCurrentMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1]
+        ]);
+        messagesQueue.shift();
+      }
+    }, 3000);
+
+    return () => clearInterval(timer);
+  }, []);
 
   useEffect(() => {
     async function fetchMessages() {
